6b310f8cd93422ec18ab88d74232fa27f4ec5b87
Andrew Sun <0xandrewsun@gmail.com>
2016-02-14 10:09:54 -0600

    Handle more notification creation methods

--- src/com/crossbowffs/debugkit/xposed/NotificationManagerHook.java
+++ src/com/crossbowffs/debugkit/xposed/NotificationManagerHook.java
@@ -2,6 +2,7 @@ package com.crossbowffs.debugkit.xposed;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.os.UserHandle;
 import com.crossbowffs.debugkit.utils.StackTraceUtils;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -12,19 +13,24 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class NotificationManagerHook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        XposedHelpers.findAndHookMethod(NotificationManager.class, "notify",
-            String.class, int.class, Notification.class, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    Notification notification = (Notification)param.args[2];
-                    String[] stacktrace = StackTraceUtils.getStackTrace();
-                    notification.extras.putStringArray("__stacktrace", stacktrace);
-                    XposedBridge.log("---------- Notification Stacktrace Start ----------");
-                    for (String element : stacktrace) {
-                        XposedBridge.log("> " + element);
-                    }
-                    XposedBridge.log("----------- Notification Stacktrace End -----------");
+        XC_MethodHook hook = new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Notification notification = (Notification)param.args[2];
+                String[] stacktrace = StackTraceUtils.getStackTrace();
+                notification.extras.putStringArray("__stacktrace", stacktrace);
+                XposedBridge.log("---------- Notification Stacktrace Start ----------");
+                for (String element : stacktrace) {
+                    XposedBridge.log("> " + element);
                 }
-            });
+                XposedBridge.log("----------- Notification Stacktrace End -----------");
+            }
+        };
+
+        XposedHelpers.findAndHookMethod(NotificationManager.class, "notify",
+            String.class, int.class, Notification.class, hook);
+
+        XposedHelpers.findAndHookMethod(NotificationManager.class, "notifyAsUser",
+            String.class, int.class, Notification.class, UserHandle.class, hook);
     }
 }
